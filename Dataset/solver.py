544d923f574f94f873db1aa9d5252993264fe879
liyeungjohn <liyeungjohn@berkeley.edu>
2014-11-10 11:39:44 -0800

    implementes overlap logic

--- Dataset/solver.py
+++ Dataset/solver.py
@@ -2,26 +2,32 @@ import networkx as nx
 from utility import *
 import sys, argparse, os
 
-def solve(reads):
-	sequence = reads[0]
-	return sequence
+class solver(object):
+	def __init__(self, input_file):
+		self.reads = parse_input(input_file)
+		self.graph = None
 
-def main(input_file):
-	reads = parse_input(input_file)
-	sequence = solve(reads)
-	output_file = make_file_name(input_file)
-	write_output(sequence, output_file)
-	return output_file
+	def solve(self):
+		sequence = self.reads[0]
+		return sequence
+
+	def main(self):
+		sequence = self.solve()
+		output_file = make_file_name(input_file)
+		write_output(sequence, output_file)
+		return output_file
 
 
-
+#parse input
 parser = argparse.ArgumentParser(description="solve hw9, cs170.")
 parser.add_argument('-i', '--input', action="store", dest="input_file", help="input file", default="")
 parser.add_argument('-s', '--solution', action="store", dest="solution_file", help="solution file", default="")
 input_file = parser.parse_args().input_file
 solution_file = parser.parse_args().solution_file
+
+#main logic
 if __name__ == "__main__":
-	output_file = main(input_file)
+	output_file = solver(input_file).main()
 	if solution_file != "":
 		check(output_file, solution_file)
 	else:
--- Dataset/utility.py
+++ Dataset/utility.py
@@ -42,6 +42,43 @@ def md5sum(filename, block_size=2**20):
 
 
 class Node(object):
-	def __init__(self):
-		self.self = self
+
+	def __init__(self, read):
+		self.read = read
+
+	@staticmethod
+	def cal_overlap(node1, node2):
+		#first direction:
+		overlap1, overlap_string1 = Node.cal_overlap_d(node1.read, node2.read)
+		#second direction:
+		overlap2, overlap_string2 = Node.cal_overlap_d(node2.read, node1.read)
+		if overlap1 > overlap2:
+			return node1, node2, overlap1, overlap_string1
+		else:
+			return node2, node1, overlap2, overlap_string2
+
+	#direction: end of node1 overlap with start of node2
+	#read1_string
+	#		read2_string
+	@staticmethod
+	def cal_overlap_d(read1, read2):
+		#truncate useless front of read1 if read1 is larger than read2
+		if len(read1) > len(read2):
+			read1 = read1[(len(read1)-len(read2)):]
+		if len(read1) == 1:
+			if read1 == read2[0]:
+				return 1, read1
+			else:
+				return 0, ""
+		#check best case of read1 entirely overlapping with read2
+		if read1 == read2[:len(read1)]:
+			return len(read1), read1
+		else:
+			return Node.cal_overlap_d(read1[1:], read2)
+
+
+class Edge(object):
+	#first_node, second_node, overlap, overlap_string
+	def __init__(self, node1, node2):
+		self.first_node, self.second_node, self.overlap, self.overlap_string = Node.cal_overlap(node1, node2)
 
